67dba12c476e72bf614a33c98c2e4a567a08c66c
JG1511 <joaotsi2024@gmail.com>
2025-03-26 16:58:43 -0300

    implementação da lógica do login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-
+import { LocalStorageService } from '../../services/local-storage.service';
+import { User } from '../../models/user';
 @Component({
   selector: 'app-login',
   imports: [ReactiveFormsModule],
@@ -10,7 +11,7 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
 export class LoginComponent {
   userForm : FormGroup;
 
-  constructor() {
+  constructor(private localStorageService: LocalStorageService) {
     this.userForm = new FormGroup({
       name: new FormControl('',Validators.required)
     });
@@ -19,7 +20,12 @@ export class LoginComponent {
   onSubmit() {
     if (!this.userForm.valid) {
       return console.error("O formulário não está válido");
+    }else{
+      const token = '1';
+      const user = new User(this.userForm.value.name);
+      this.localStorageService.setLocalStorage(token, user);
+      console.log('Usuário salvo no local storage');
     }
-    
+
   }
 }
